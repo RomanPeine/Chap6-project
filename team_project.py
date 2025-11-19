6b34f7665730db800ef106b0a732d52ff23290aa
Roman_Peine <2017060@student.goddardusd.com>
2025-11-19 12:51:34 -0600

    Create team_project.py

---
+++ team_project.py
@@ -0,0 +1,25 @@
+def menu():
+    
+def main():
+    while keep_going.lower() = "y" or keep_going.lower() == "yes":
+        choice = menu()
+        if choice == 1:
+            add_contact()
+        if choice == 2:
+            search_contact()
+        if choice == 3
+            edit_contact()
+        if choice == 4:
+            delete_contact()
+        if choice == 5:
+            display_contact()
+        keep_going == input("\nDo you want to use the menu again if so enter y or yes: ")
+def add_contact():
+    
+def search_contact()
+
+def edit_contact()
+
+def delete_contact():
+    
+def display_contact():
