08a0ddf22f0a4cf562f435a8c3e0d1c687497df6
Roman_Peine <2017060@student.goddardusd.com>
2025-11-20 11:54:28 -0600

    Update team_project.py

--- team_project.py
+++ team_project.py
@@ -1,6 +1,13 @@
 def menu():
+    #menu accepts no arguments
+    #prompts the user to choose an option
+    #returns the users choice
+    pass
     
 def main():
+    #accepts no arguments
+    #runs the contact program
+    #calls all other programs
     while keep_going.lower() = "y" or keep_going.lower() == "yes":
         choice = menu()
         if choice == 1:
@@ -14,12 +21,30 @@ def main():
         if choice == 5:
             display_contact()
         keep_going == input("\nDo you want to use the menu again if so enter y or yes: ")
-def add_contact():
+        pass
     
-def search_contact()
+def add_contact():
+    #accepts no arguements
+    #adds a contact to the contact list\
+    pass
+    
+def search_contact():
+    #accepts no arguements
+    #searches and pulls info for a specific contact
+    #displays the info
+    pass
 
-def edit_contact()
+def edit_contact():
+    #accepts no arguements
+    #edits a contacts info
+    pass
 
 def delete_contact():
-    
+    #accepts no arguements
+    #deletes a contact
+    pass
+
 def display_contact():
+    #accepts no arguements
+    #displays all contacts
+    pass
